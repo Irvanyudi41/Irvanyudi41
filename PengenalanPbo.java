c72d5018ee5e13b98f7a6b795c8ef1bd9cb14ee1
Irvanyudi41 <114343449+Irvanyudi41@users.noreply.github.com>
2022-09-26 17:44:14 +0700

    Add files via upload

---
+++ PengenalanPbo.java
@@ -0,0 +1,14 @@
+/*
+Nama  : Irvan Yudistiansyah
+NIM   : 20210040082
+Kelas : TI21E
+Tugas Sesi 1
+ */
+public class PengenalanPbo {
+    public static void main(String[] args) {
+      System.out.println("BELAJAR JAVA");
+      System.out.println("SANGAT MUDAH SEKALI");
+      System.out.println("dan sangat menyenangkan");  
+    }
+      
+}
---
+++ PengenalanPbo2.java
@@ -0,0 +1,17 @@
+
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
+/*
+Nama  : Irvan Yudistiansyah
+NIM   : 20210040082
+Kelas : TI21E
+ */
+public class PengenalanPbo2 {
+    public static void main(String[] args) {
+        String input;
+        input = JOptionPane.showInputDialog("Anda Sedang Belajat Apa?");
+        JOptionPane.showMessageDialog(null, "Belajar "+input+" Sangat Mudah","message",JOptionPane.INFORMATION_MESSAGE);
+        
+    }
+}
